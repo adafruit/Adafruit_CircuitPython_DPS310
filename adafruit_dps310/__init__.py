5a2c1faea76aa539a447c2db8e1fca0da22504f9
foamyguy <foamyguy@gmail.com>
2022-01-09 16:12:04 -0600

    sphinx fix for docstring

--- adafruit_dps310/__init__.py
+++ adafruit_dps310/__init__.py
@@ -1,17 +1,13 @@
-# SPDX-FileCopyrightText: 2022 Tim Cocks for Adafruit Industries
+# SPDX-FileCopyrightText: 2020 Bryan Siepert for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
 """
 `adafruit_dps310`
 ================================================================================
+
 Library for the DPS310 Precision Barometric Pressure Sensor
 
 * Author(s): Bryan Siepert, Jose David M.
-Implementation Notes
---------------------
-**Software and Dependencies:**
-* Adafruit CircuitPython firmware for the supported boards:
-  https://github.com/adafruit/circuitpython/releases
 """
 from adafruit_dps310.advanced import DPS310_Advanced as DPS310
