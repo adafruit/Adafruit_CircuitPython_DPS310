978b6fe8495c1076f8799a55a4bc42e68b871eec
foamyguy <foamyguy@gmail.com>
2022-01-09 16:04:25 -0600

    add copyright and comment for CI checks

--- adafruit_dps310/__init__.py
+++ adafruit_dps310/__init__.py
@@ -1,1 +1,16 @@
+# SPDX-FileCopyrightText: 2022 Tim Cocks for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_dps310`
+================================================================================
+Dynamic progress bar widget for CircuitPython displays
+* Author(s): Bryan Siepert, Jose David M.
+Implementation Notes
+--------------------
+**Software and Dependencies:**
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+"""
 from adafruit_dps310.advanced import DPS310_Advanced as DPS310
